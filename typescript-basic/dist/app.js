f0a2f48a4ef1e61613b6cc6827401206ec7f7818
liuduwei <17341730328@163.com>
2023-06-21 11:30:54 +0800

    complete task advanced-types

--- typescript-basic/dist/app.js
+++ typescript-basic/dist/app.js
@@ -1,15 +1,24 @@
 "use strict";
-function iAm(person) {
-    if ("school" in person) {
-        console.log(person.school);
-    }
-    if ("age" in person) {
-        console.log(person.age);
+function iAm2(person) {
+    switch (person.type) {
+        case "Student":
+            console.log("im a student");
+        case "Teenagers":
+            console.log("im a teenagers");
     }
 }
-var liuduwei = {
-    school: "uestc",
-    age: 22,
+var tongtong = {
+    type: "Teenagers",
+    age: 2,
 };
-iAm(liuduwei);
+iAm2(tongtong);
+var h1 = document.querySelector("h1");
+console.log(h1);
+var input = document.getElementById("input");
+console.log(input);
+var value = input.value;
+console.log(value);
+var c = {
+    id: 2,
+};
 //# sourceMappingURL=app.js.map
